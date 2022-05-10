b42b946567041dba1b09f74d0d11ba5bb598f504
Sandeep Zechariah George <sandeepzgk@gmail.com>
2022-05-10 14:36:58 -0700

    partially working

--- server/vt-haptics-platform/handler.js
+++ server/vt-haptics-platform/handler.js
@@ -1,11 +1,10 @@
 const AWS = require("aws-sdk");
 const Validator = require('jsonschema').Validator;
-
-
 const express = require("express");
 const serverless = require("serverless-http");
-const db_schema = require("./server_schema.json");
-const web_schema = require("./web_schema.json");
+
+const db_schema = require("./schema.json");
+const web_schema = require("./schema.json");
 
 const app = express();
 
@@ -15,35 +14,34 @@ const dynamoDbClientParams = {};
 if (process.env.IS_OFFLINE) {
   dynamoDbClientParams.region = 'localhost'
   dynamoDbClientParams.endpoint = 'http://localhost:8000'
-
   AWS.config.update({
     region: 'localhost',
     accessKeyId: 'xxxxxxxxxxxxxx',
     secretAccessKey: 'xxxxxxxxxxxxxx',
-    });
+});
 }
 const dynamoDbClient = new AWS.DynamoDB.DocumentClient(dynamoDbClientParams);
 
 app.use(express.json());
 
-app.post("/createExperiment", async function (req, res) {
-  var instance = req.body;
-  var v = new Validator();
-  var validation_result = v.validate(instance,web_schema)
-  if(validation_result.valid==True)
-  {
 
-  }
-  else
-  {
-    res.status(400).json({ error: "Could not retreive user",result:validation_result.errors });
-  }
-
+app.post("/users", async function (req, res) {
+    var instance = req.body;
+    var v = new Validator();
+    var validation_result = v.validate(instance,web_schema)
+    if(validation_result.valid)
+    {
+  
+    }
+    else
+    {
+      res.status(400).json({ error: "Could not retreive user",result:validation_result.errors });
+    }
   const params = {
     TableName: USERS_TABLE,
     Item: {
-      userId: userId,
-      name: name,
+      userId: "ASaa",
+      name: "test",
     },
   };
 
