e6ac43b947ad42746dc52b3bdac7853ee888d93c
chen716 <chen716@usc.edu>
2022-05-11 17:02:32 -0700

    Yang's been here

--- server/vt-haptics-platform/handler.js
+++ server/vt-haptics-platform/handler.js
@@ -7,9 +7,25 @@ const db_schema = require("./schema.json");
 const web_schema = require("./schema.json");
 
 const app = express();
-
+String.prototype.hashCode = function(){
+  var hash = 0;
+  for (var i = 0; i < this.length; i++) {
+      var code = this.charCodeAt(i);
+      hash = ((hash<<5)-hash)+code;
+      hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
 const USERS_TABLE = process.env.USERS_TABLE;
-
+String.prototype.hashCode = function(){
+    var hash = 0;
+    for (var i = 0; i < this.length; i++) {
+        var code = this.charCodeAt(i);
+        hash = ((hash<<5)-hash)+code;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
 const dynamoDbClientParams = {};
 if (process.env.IS_OFFLINE) {
     dynamoDbClientParams.region = 'localhost'
@@ -35,49 +51,53 @@ app.post("/setExperiment", async function(req, res) {
         
 
         try {
+          //Todo: generate a hash with 6 bit and check theres no collision
+          var hashed = false;
+          while(!hashed){
+            var email = req.body.email
+            email = email.substr(email.length - 6)
+            var time = String(Date.now())
+            time  = time.substring(time.length - 6)
+            
+            var hash = (email + time).hashCode()
+            console.log("time"+time +" hashs: "+ hash)
+            //Check if the hash exist in the database, if so, generate another and try again.
+
+
+
             const getParams = {
                 TableName: USERS_TABLE,
                 // Key: {
                 //   email: req.body.email,
                 // },
-                FilterExpression: 'email = :email ',
+                FilterExpression: 'email = :email and hashs = :hashs',
                 ExpressionAttributeValues: {
-                  ':email': req.body.email
+                  ':email': req.body.email,
+                  ':hashs': hash
                 }
                 
               };
-           //console.log(48)
-            var params;
-            const Item= await dynamoDbClient.scan(getParams).promise().then(
-              data =>{ console.log(data.Count);
-              if(data.Count > 0){
-                var toEdit = data.Items[0];
-                params = {
-              
-                  TableName: USERS_TABLE,
-                    Item: instance,
-                };
-                toEdit.haptic_setup.push(instance.haptic_setup[0])
-                console.log(toEdit.haptic_setup)
-                params = {
-              
-                  TableName: USERS_TABLE,
-                    Item: toEdit,
-                };
-              }
-              else{
-                console.log(62)
-                params = {
-              
-                  TableName: USERS_TABLE,
-                    Item: instance,
-        
-                    
-                };
+            //console.log(48)
+              var params;
+              const Item= await dynamoDbClient.scan(getParams).promise().then(
+                data =>{ console.log(data.Count);
+                if(data.Count ==  0){
+                  console.log(62)
+                  instance.hashs = String(hash)
+                  hashed = true;
+                  params = {
                 
-              }
-              }
-              );
+                    TableName: USERS_TABLE,
+                      Item: instance,
+                    
+                      
+                  };
+                  
+                }
+                }
+                );
+            }
+            console.log(params.Item)
               await dynamoDbClient.put(params).promise();
             
             
@@ -102,33 +122,69 @@ app.post("/setExperiment", async function(req, res) {
     
 });
 
-app.post("/getExperiment", async function(req, res) {
-    const params = {
-        TableName: USERS_TABLE,
-        Key: {
-          email: req.body.email,
-        },
-        
-      };
-    
-      try {
-        const { Item } = await dynamoDbClient.get(params).promise();
-        
-        if (Item) {
-            
-          
+app.post("/getByEmail", async function(req, res) {
+    const getParams = {
+      TableName: USERS_TABLE,
+      FilterExpression: 'email = :email',
+      ExpressionAttributeValues: {
+        ':email': req.body.email
+      }
+      
+    }
+    console.log("getParams: "+ getParams.ExpressionAttributeValues+" email: "+ req.body.email);
 
-          res.json(Item );
-        } else {
-          res
+      try {
+        const Item= await dynamoDbClient.scan(getParams).promise().then(
+          data =>{
+          if(data.Count > 0){
+            res.json(data.Items);
+            
+          }
+          else{
+            res
             .status(404)
-            .json({ error: 'Could not find user with provided "userId"' });
-        }
+            .json({ error: 'Could not find user with provided "userId"' });}
+          }
+        );
+    
       } catch (error) {
         console.log(error);
         res.status(500).json({ error: "Could not retreive user" });
       }
     
+});
+
+app.post("/getById", async function(req, res) {
+  const getParams = {
+    TableName: USERS_TABLE,
+    // Key: {
+    //   email: req.body.email,
+    // },
+    FilterExpression: 'hashs = :hashs ',
+    ExpressionAttributeValues: {
+      ':hashs': req.body.hashs
+    }
+    
+  }
+    try {
+      const Item= await dynamoDbClient.scan(getParams).promise().then(
+        data =>{
+        if(data.Count > 0){
+          res.json(data.Items[0] );
+          
+        }
+        else{
+          res
+          .status(404)
+          .json({ error: 'Could not find user with provided "userId"' });}
+        }
+      );
+  
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Could not retreive user" });
+    }
+  
 });
 
 app.use((req, res, next) => {
