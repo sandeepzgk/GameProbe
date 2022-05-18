858a0728dca844bd547288950f449c887ab35ebe
Ubuntu <ubuntu@ip-172-31-18-88.ec2.internal>
2022-05-18 22:33:23 +0000

    updated looping error in uuid creation and file upload

--- server/handler.js
+++ server/handler.js
@@ -2,19 +2,22 @@ const AWS = require("aws-sdk");
 const Validator = require("jsonschema").Validator;
 const express = require("express");
 const serverless = require("serverless-http");
-AWS.config.update({
+AWS.config.update(
+{
     region: "us-east-1"
-  });
+});
 const db_schema = require("./static/assets/formschema.json");
-
 const app = express();
 const DATA_TABLE = process.env.DATA_TABLE;
 const STORAGE_BUCKET = process.env.STORAGE_BUCKET;
 const s3 = new AWS.S3();
+const debug = True;
 
-String.prototype.hashCode = function() {
+String.prototype.hashCode = function()
+{
     var hash = 0;
-    for (var i = 0; i < this.length; i++) {
+    for (var i = 0; i < this.length; i++)
+    {
         var code = this.charCodeAt(i);
         hash = ((hash << 5) - hash) + code;
         hash = hash & hash; // Convert to 32bit integer
@@ -24,71 +27,94 @@ String.prototype.hashCode = function() {
 
 const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 
+function log(str)
+{
+    if (debug)
+    {
+        console.log(str);
+    }
+}
+
 
 app.use(express.json());
-app.post("/setExperiment" ,async function(req, res) {
+app.post("/setExperiment", async function(req, res)
+{
     var instance = req.body;
     var v = new Validator();
     var validation_result = v.validate(instance, db_schema);
-    if (validation_result.valid) {
-        try {
-            //Todo: generate a hash with 6 bit and check theres no collision
+    if (validation_result.valid)
+    {
+        log("Validation Success");
+        try
+        {
             var hashed = false;
-            while (!hashed) {
+            while (!hashed)
+            {
                 var email = req.body.email;
-                email = email.substr(0, email.length<4?email.length:4); 
+                email = email.substr(0, email.length < 4 ? email.length : 4);
                 var time = String(Date.now());
                 time = time.substring(time.length - 4);
-                var hash = (email + time).hashCode();
-                //Check if the hash exist in the database, if so, generate another and try again.
+                var uid = (email + time).hashCode();
+                log("Computed UUID:" + uid);
+                //Check if the generated uid is genuinely uuid (i.e not exisiting in the database)
                 const getParams = {
                     TableName: DATA_TABLE,
-                    FilterExpression: "email = :email and uuid = :uuid",
-                    ExpressionAttributeValues: {
+                    FilterExpression: "email = :email and uuid = :uid",
+                    ExpressionAttributeValues:
+                    {
                         ":email": req.body.email,
-                        ":uuid": hash
+                        ":uuid": uid
                     }
                 };
-     
-                var params;
-                const Item = await dynamoDbClient.scan(getParams).promise().then(
-                    data => {
-                        if (data.Count == 0) {
 
-                            instance.uuid = String(hash);
+                var putParams;
+                const Item = await dynamoDbClient.scan(getParams).promise().then(
+                    getData =>
+                    {
+                        if (getData.Count == 0)
+                        {
+
+                            instance.uuid = String(uid);
                             hashed = true;
-                            params = {
+                            putParams = {
                                 TableName: DATA_TABLE,
                                 Item: instance,
                             };
                         }
                     }
                 );
-                await s3
-                  .upload({
-                      Bucket: STORAGE_BUCKET,
-                      Key: String(hash)+"/long",
-                      Body: instance.haptic_setup[0].linked_files.long_effect
-                    }).promise()
-               
-                await s3
-                  .upload({
-                      Bucket: STORAGE_BUCKET,
-                      Key: String(hash)+"-short",
-                      Body: instance.haptic_setup[0].linked_files.short_effect
-                    }).promise()
-               
             }
+            await s3.upload(
+            {
+                Bucket: STORAGE_BUCKET,
+                Key: String(hash) + "/long",
+                Body: instance.haptic_setup[0].linked_files.long_effect
+            }).promise();
+
+            await s3.upload(
+            {
+                Bucket: STORAGE_BUCKET,
+                Key: String(hash) + "/short",
+                Body: instance.haptic_setup[0].linked_files.short_effect
+            }).promise();
+
+
             await dynamoDbClient.put(params).promise();
             res.status(200);
-        } catch (error) {
+        }
+        catch (error)
+        {
             console.log(error)
-            res.status(500).json({
+            res.status(500).json(
+            {
                 error: "Could not create user"
             });
         }
-    } else {
-        res.status(400).json({
+    }
+    else
+    {
+        res.status(400).json(
+        {
             error: "Validation Error",
             validation_result: validation_result.errors
         });
@@ -96,83 +122,112 @@ app.post("/setExperiment" ,async function(req, res) {
     res.end()
 });
 
-app.post("/getByEmail", async function(req, res) {
+app.post("/getByEmail", async function(req, res)
+{
     const getParams = {
         TableName: DATA_TABLE,
         FilterExpression: "email = :email",
-        ExpressionAttributeValues: {
+        ExpressionAttributeValues:
+        {
             ":email": req.body.email
         }
     }
 
-    try {
+    try
+    {
         const Item = await dynamoDbClient.scan(getParams).promise().then(
-            data => {
-                if (data.Count > 0) {
+            data =>
+            {
+                if (data.Count > 0)
+                {
                     res.json(data.Items);
-                } else {
-                    res.status(404).json({
+                }
+                else
+                {
+                    res.status(404).json(
+                    {
                         error: "Could not find user with provided id"
                     });
                 }
             }
         );
-    } catch (error) {
-        res.status(500).json({
+    }
+    catch (error)
+    {
+        res.status(500).json(
+        {
             error: "Could not retreive user"
         });
     }
 });
 
-app.post("/getById", async function(req, res) {
+app.post("/getById", async function(req, res)
+{
     const getParams = {
         TableName: DATA_TABLE,
         FilterExpression: "uuid = :uuid ",
-        ExpressionAttributeValues: {
+        ExpressionAttributeValues:
+        {
             ":uuid": req.body.uuid
         }
     }
-    try {
+    try
+    {
         const Item = await dynamoDbClient.scan(getParams).promise().then(
-            data => {
-                if (data.Count > 0) {
+            data =>
+            {
+                if (data.Count > 0)
+                {
                     res.json(data.Items[0]);
-                } else {
-                    res.status(404).json({
+                }
+                else
+                {
+                    res.status(404).json(
+                    {
                         error: "Could not find experiment with provided id"
                     });
                 }
             }
         );
-    } catch (error) {
-        res.status(500).json({
+    }
+    catch (error)
+    {
+        res.status(500).json(
+        {
             error: "Could not retreive user"
         });
     }
 });
 
-app.post("/getFile", async function(req,res){
+app.post("/getFile", async function(req, res)
+{
     var bucketParams = {
-        Bucket: STORAGE_BUCKET,  
+        Bucket: STORAGE_BUCKET,
         Key: req.body.filename
     };
-    s3.getObject(bucketParams, function(err, data) {
+    s3.getObject(bucketParams, function(err, data)
+    {
         // Handle any error and exit
-        if (err){
+        if (err)
+        {
             console.log(err)
             return err;
         }
-      let objectData = data.Body.toString('utf-8'); // Use the encoding necessary
+        let objectData = data.Body.toString('utf-8'); // Use the encoding necessary
     });
     res.status(200);
 
-    
+
 
 })
-app.use((req, res, next) => {
-    return res.status(404).json({
+app.use((req, res, next) =>
+{
+    return res.status(404).json(
+    {
         error: "Not Found",
     });
 });
+
+
 
 module.exports.handler = serverless(app);
