e374e602ee6176f1a14135bc5f99eaeeaaa09e04
Sandeep Zechariah George <sandeepzgk@gmail.com>
2022-05-17 17:57:08 -0700

    un-tested-cleanup

--- server/vt-haptics-platform/handler.js
+++ server/vt-haptics-platform/handler.js
@@ -3,16 +3,10 @@ const Validator = require("jsonschema").Validator;
 const express = require("express");
 const serverless = require("serverless-http");
 AWS.config.update({
-    //accessKeyId: 'AKIAZTPKKP6ANO7VKR7Y' ,
-    //secretAccessKey: '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' ,
-    region: "us-east-1",
-    //endpoint: 'http://localhost:8000',
+    region: "us-east-1"
   });
-//AWS.config.update({region: 'REGION'});// Create DynamoDB document client
-//v//ar docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 const db_schema = require("./schema.json");
 
-//const { S3Client, PutObjectCommand, S3 } = require("@aws-sdk/client-s3");
 const app = express();
 const USERS_TABLE = process.env.USERS_TABLE;
 const s3 = new AWS.S3();
@@ -26,18 +20,8 @@ String.prototype.hashCode = function() {
     }
     return Math.abs(hash);
 }
-const dynamoDbClientParams = {};
-// if (process.env.IS_OFFLINE) {
-    dynamoDbClientParams.region = "us-east-1";
-    // dynamoDbClientParams.endpoint = "http://localhost:8000";
-    // AWS.config.update({
-    //     region: "localhost",
-    //     endpoint: 'http://localhost:8000',
-    //     accessKeyId: "xxxxxxxxxxxxxx",
-    //     secretAccessKey: "xxxxxxxxxxxxxx",
-    //});
-//}
-const dynamoDbClient = new AWS.DynamoDB.DocumentClient()//dynamoDbClientParams);
+
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 
 
 app.use(express.json());
@@ -65,15 +49,7 @@ app.post("/setExperiment" ,async function(req, res) {
                         ":hashs": hash
                     }
                 };
-                // const getParams = {
-                //     TableName: USERS_TABLE,
-                //     Key: {
-	
-                //         ":hashs": hash,
-                      
-                //       },
-                // };
-                console.log(hash);
+     
                 var params;
                 const Item = await dynamoDbClient.scan(getParams).promise().then(
                     data => {
@@ -89,70 +65,19 @@ app.post("/setExperiment" ,async function(req, res) {
                         }
                     }
                 );
-                // await dynamoDbClient.get(getParams, (error, result) => {
-	
-                //     if (error) {
-                    
-                //       console.log("88: "+error);
-                    
-                //       res.status(400).json({ error: 'Could not get user' });
-                    
-                //     }
-                //     console.log(101);
-                //     if (result.Item) {
-                    
-                //       print("found user ");
-
-                //     //res.json(result.Item);
-                //       //res.json({ userId, name });
-                    
-                //     } else {
-                        
-                //             instance.hashs = String(hash);
-                //             hashed = true;
-                //             params = {
-                //                 TableName: USERS_TABLE,
-                //                 Item: instance,
-                //             };
-                        
-                //     // res.status(404).json({ error: "User not found" });
-                //     console.log("No user found ");
-                //     }
-                    
-                //   }).promise();
-                console.log(81);
-                //console.log(88)
-                console.log(instance.haptic_setup[0].linked_files.long_effect+String(hash)+"-long")
-                console.log(instance.haptic_setup[0].linked_files.short_effect+String(hash)+"-short")
-               // console.log(88)
                 await s3
                   .upload({
                       Bucket: "haptic-bucket",
                       Key: String(hash)+"-long",
                       Body: instance.haptic_setup[0].linked_files.long_effect
-                    }
-                      //Buffer.from("abcd"),
-                    ).promise()
-	                //.then(() => callback(null, "ok"));
+                    }).promise()
                
                 await s3
                   .upload({
                       Bucket: "haptic-bucket",
                       Key: String(hash)+"-short",
-                      Body: instance.haptic_setup[0].linked_files.short_effect,
-                      ACL: 'public-read'
-                    }
-                      //Buffer.from("abcd"),
-                    ).promise()
-                // s3
-                //     .upload(
-                //       new PutObjectCommand({
-                //         Bucket: "local-bucket",
-                //         Key: String(hash)+"-short",
-                //         Body: instance.haptic_setup[0].linked_files.short_effect
-                //         //Buffer.from("abcd"),
-                //       }))
-                      //.then(() => callback(null, "ok"));
+                      Body: instance.haptic_setup[0].linked_files.short_effect
+                    }).promise()
                
             }
             await dynamoDbClient.put(params).promise();
@@ -228,7 +153,6 @@ app.post("/getById", async function(req, res) {
 });
 
 app.post("/getFile", async function(req,res){
-    console.log("file",req.body.filename);
     var bucketParams = {
         Bucket: "haptic-bucket",  
         Key: req.body.filename
@@ -240,17 +164,10 @@ app.post("/getFile", async function(req,res){
             console.log(195)
             return err;
         }
-      // No error happened
-      // Convert Body from a Buffer to a String
       let objectData = data.Body.toString('utf-8'); // Use the encoding necessary
-      console.log(199)
-      console.log(objectData);
     });
     res.status(200);
-        /*bucketParams)
-            .createReadStream()
-            .pipe(res);
-*/
+
     
 
 })
