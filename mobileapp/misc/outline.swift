682188be9dcdbf0ff3901754f08357795f4e9f53
Sandeep Zechariah George <sandeepzgk@gmail.com>
2022-05-24 13:52:22 -0700

    outline prototypes for functions

---
+++ mobileapp/misc/outline.swift
@@ -0,0 +1,72 @@
+/**
+DESCR: Initializes the LofeltHaptics Suite
+INPUT: na
+RET  : the function returns true, if it successfully intialized or false if it failed to initialize.
+**/
+
+func setupHaptics() 
+{
+
+}
+
+
+/**
+DESCR: Check if the iOS meets minimum requirements to make sure it works.
+INPUT: na
+RET  : return true if it meets requirement, else return false. Important for the game engine to know that it does not qualify to use this particular device for the experiment so a error should be shown to the user. 
+DOC: https://developer.lofelt.com/integrating-haptics/studio-framework-for-ios/#integrating-haptics-using-the-studio-framework-for-ios  deviceMeetsMinimumRequirements
+func checkDevRequirements()
+{
+
+}
+
+/**
+DESCR: Load Haptic Data and Store them for use
+INPUT: (data: String)
+RET  : the function returns true, if it successfully loaded the haptic data or false
+**/
+func loadHapticData(data: String) 
+{
+
+}
+
+
+
+/**
+DESCR: Play Haptic Data
+INPUT: reference to the previously loaded haptic data
+RET  : the function returns true, it success or false
+**/
+func playHapticData(data: !!!!TYPE to be DETERMINED) 
+{
+
+}
+
+
+/**
+DESCR: Play Haptic Data 
+INPUT: reference to the previously loaded haptic data
+RET  : the function returns true, it success or false
+DOC: https://developer.lofelt.com/integrating-haptics/studio-framework-for-ios/#play-haptic-with-audio
+**/
+func playHapticData(data: NSString) 
+{
+
+}
+
+
+///// to gcerate swift struct from JSON, use https://app.quicktype.io/
+
+
+
+
+/**
+DESCR: Requests and downloads the JSON config file from our server
+INPUT: Requires, two parameters, one is the postURL 
+RET  : the function returns true, it success or false
+DOC: https://developer.lofelt.com/integrating-haptics/studio-framework-for-ios/#play-haptic-with-audio
+**/
+func requestJSONConfig(postURL:URL, expID:NSString) 
+{
+
+}
